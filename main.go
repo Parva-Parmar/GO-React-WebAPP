831fab4f68c63960a8ecb1a8e32d797f8c477df8
Parva Parmar <parva004cp@gmail.com>
2024-06-10 11:53:11 +0530

    updated ui

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 )
 
 type Todo struct {
-	ID        primitive.ObjectID    `json:"id,omitempty" bson:"_id.omitempty"`
+	ID        primitive.ObjectID    `json:"_id,omitempty" bson:"_id.omitempty"`
 	Completed bool   `json:"completed"`
 	Body      string `json:"body"`
 }
@@ -24,11 +24,13 @@ var collection *mongo.Collection
 
 func main() {
 	fmt.Println("hello world")
-
-	err := godotenv.Load(".env")
-	if err != nil{
-		log.Fatal("Error loading .env file:",err)
+	if os.Getenv("ENV") != "production"{
+		err := godotenv.Load(".env")
+		if err != nil{
+			log.Fatal("Error loading .env file:",err)
+		}
 	}
+	
 
 	MONGODB_URI := os.Getenv("MONGODB_URI")
 	clientOptions := options.Client().ApplyURI(MONGODB_URI)
@@ -50,6 +52,11 @@ func main() {
 
 	app := fiber.New()
 
+	// app.Use(cors.New(cors.Config{
+	// 	AllowOrigins: "http://localhost:5173",
+	// 	AllowHeaders: "Origin,Content-Type,Accept",
+	// }))
+
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodos)
 	app.Patch("/api/todos/:id", updateTodos)
@@ -58,6 +65,10 @@ func main() {
 	port := os.Getenv("PORT")
 	if port == ""{
 		port = "5000"
+	}
+
+	if os.Getenv("ENV") == "production" {
+		app.Static("/","./client/dist")
 	}
 	log.Fatal(app.Listen("0.0.0.0:"+port))
 }
